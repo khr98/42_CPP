a4d2adf050ea4cabf3b990cffd7ab416e7c10b7b
hyerim Kim <hyerim980513@gmail.com>
2021-09-30 21:11:47 +0900

    continue

---
+++ CPP01/ex01/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    char *str;
+
+    str = argv[1];
+    switch (str)
+    {
+        case "EXIT" :
+            break;
+        case "SEARCH" :
+            break;
+        case "ADD" :
+            break;
+        default:
+            break;
+    }
+}
